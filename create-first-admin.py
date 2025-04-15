2207034427f3d64ed818f5b6e6f0f5f26faa53c6
anas <160654451+anascl11@users.noreply.github.com>
2025-04-15 10:27:32 +0100

    Update create-first-admin.py

--- create-first-admin.py
+++ create-first-admin.py
@@ -1,16 +1,16 @@
 from werkzeug.security import generate_password_hash
 import mysql.connector
 
-# Connect to DB
+# connect to DB
 conn = mysql.connector.connect(
-    host="192.168.10.4",
-    user="anas",
-    password="123",
-    database="sms_gateway"
+    host = "192.168.10.6",
+    user = "atrait",
+    password = "atrait11!!",
+    database = "sms_gateway"
 )
 cursor = conn.cursor()
 
-# Create admin
+# create admin
 username = 'admin'
 password = generate_password_hash('admin', method='pbkdf2:sha256')
 
